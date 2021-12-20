f2061ca144cd7671d846780e18e6776226d9bc79
Charisma Gochenour <charismagochenour@gmail.com>
2021-12-20 11:52:28 -0600

    All the work to this point

--- CourseLibrary.API/Controllers/AuthorsController.cs
+++ CourseLibrary.API/Controllers/AuthorsController.cs
@@ -1,4 +1,8 @@
-﻿using CourseLibrary.API.Services;
+﻿using AutoMapper;
+using CourseLibrary.API.Helpers;
+using CourseLibrary.API.Models;
+using CourseLibrary.API.ResourceParameters;
+using CourseLibrary.API.Services;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -11,27 +15,75 @@ namespace CourseLibrary.API.Controllers
   [Route("api/authors")]
   public class AuthorsController : ControllerBase
   {
-    // example comment and commit
     private readonly ICourseLibraryRepository _courseLibraryRepository;
+    private readonly IMapper _mapper;
 
-    public AuthorsController(ICourseLibraryRepository courseLibraryRepository)
+    public AuthorsController(ICourseLibraryRepository courseLibraryRepository,
+        IMapper mapper)
     {
       _courseLibraryRepository = courseLibraryRepository ??
-        throw new ArgumentNullException(nameof(courseLibraryRepository));
+          throw new ArgumentNullException(nameof(courseLibraryRepository));
+      _mapper = mapper ??
+          throw new ArgumentNullException(nameof(mapper));
     }
 
     [HttpGet()]
-    public IActionResult GetAuthors()
+    [HttpHead]
+    public ActionResult<IEnumerable<AuthorDto>> GetAuthors(
+        [FromQuery] AuthorsResourceParameters authorsResourceParameters)
     {
-      var authorsFromRepo = _courseLibraryRepository.GetAuthors();
-      return new JsonResult(authorsFromRepo);
+      var authorsFromRepo = _courseLibraryRepository.GetAuthors(authorsResourceParameters);
+      return Ok(_mapper.Map<IEnumerable<AuthorDto>>(authorsFromRepo));
     }
 
-    [HttpGet("{authorId}")]
-    public IActionResult GetAuthor()
+    [HttpGet("{authorId}", Name = "GetAuthor")]
+    public IActionResult GetAuthor(Guid authorId)
     {
+      var authorFromRepo = _courseLibraryRepository.GetAuthor(authorId);
 
+      if (authorFromRepo == null)
+      {
+        return NotFound();
+      }
+
+      return Ok(_mapper.Map<AuthorDto>(authorFromRepo));
     }
 
+    [HttpPost]
+    public ActionResult<AuthorDto> CreateAuthor(AuthorForCreationDto author)
+    {
+      var authorEntity = _mapper.Map<Entities.Author>(author);
+      _courseLibraryRepository.AddAuthor(authorEntity);
+      _courseLibraryRepository.Save();
+
+      var authorToReturn = _mapper.Map<AuthorDto>(authorEntity);
+      return CreatedAtRoute("GetAuthor",
+          new { authorId = authorToReturn.Id },
+          authorToReturn);
+    }
+
+    [HttpOptions]
+    public IActionResult GetAuthorsOptions()
+    {
+      Response.Headers.Add("Allow", "GET,OPTIONS,POST");
+      return Ok();
+    }
+
+    [HttpDelete("{authorId}")]
+    public ActionResult DeleteAuthor(Guid authorId)
+    {
+      var authorFromRepo = _courseLibraryRepository.GetAuthor(authorId);
+
+      if (authorFromRepo == null)
+      {
+        return NotFound();
+      }
+
+      _courseLibraryRepository.DeleteAuthor(authorFromRepo);
+
+      _courseLibraryRepository.Save();
+
+      return NoContent();
+    }
   }
 }
